65fd15be5b9595a8dc1dcaf8eb5fb69001bbb762
Yashvi Gaur <yashvigaur2111@gmail.com>
2024-11-14 12:53:28 +0530

    service section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header.jsx";
+import ServicePage from "../components/Service.jsx";
 
 function Home() {
   return (
     <>
       <Header />
+      <ServicePage/>
     </>
   );
 }
