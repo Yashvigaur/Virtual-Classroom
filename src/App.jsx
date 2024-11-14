278f69d1cef1dc5aaf9f3de1831a4e0e84a1f33e
Yashvi Gaur <yashvigaur2111@gmail.com>
2024-11-14 14:06:37 +0530

    Service

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { Routes,Route } from 'react-router-dom'
-import './App.css'
-import Login from './pages/Login'
-import Signup from "./pages/SignUp.jsx"
+import Signup from "./pages/SignUp.jsx";
+import Navbar from "./components/Navbar.jsx";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home.jsx";
 function App() {
-  
-
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Signup/>}/>
-      <Route path="/login" element={<Login/>}/>
-
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header.jsx";
+import ServicePage from "../components/Service.jsx";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <ServicePage/>
+    </>
+  );
+}
+
+export default Home;
